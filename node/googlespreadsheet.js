9f04baa67f04edc3637c428b2974195472fe752a
Hiroshi Sano <hrs.sano645@gmail.com>
2021-03-10 22:42:07 +0900

    fix spreadsheet id

--- node/googlespreadsheet.js
+++ node/googlespreadsheet.js
@@ -12,7 +12,7 @@ const fetchCSVfromGoogleSpreadSheet = async function(key) {
 }
 
 const main = async function() {
-  const key = '2PACX-1vSFMNp5HcRNOF5MrAujEUWR1dIoX2mncMEWTbPlVAaJqKWiq831-6gnCyI7n_G8YfPqNQXrfwyVjyHL'
+  const key = '2PACX-1vS92FFvTP6VBPu-ZHnJOYitgzQY0gyKCiOBWJoyLkkV4lIQ4Se6rGxgMVP7pFLOoG4ywi7l43TR0Ou9'
   const data = await fetchCSVfromGoogleSpreadSheet(key)
   console.log(data)
 }
